b67074ba459912101f905e4150b474828bba20cc
xzcxCDL <1952932103@qq.com>
2022-07-15 21:07:28 +0800

    revert 1.0

--- src/com/atguigu/java/HelloJava.java
+++ src/com/atguigu/java/HelloJava.java
@@ -3,5 +3,6 @@ package com.atguigu.java;
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("哈哈哈");
+        System.out.println("哈哈哈333333");
     }
 }
